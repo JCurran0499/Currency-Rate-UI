cf98f38b9a85fca6323e02f2677a08487f7d5bcb
Jack Curran <jackurran49@gmail.com>
2023-07-22 21:05:50 -0400

    updating lambdas

--- app/src/routes/country/Country.js
+++ app/src/routes/country/Country.js
@@ -1,16 +1,70 @@
 import { useParams, Navigate } from 'react-router-dom'
 import { symbols, codes } from '../../util/constants'
+import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 import './Country.css'
 
 export const Country = () => {
     const params = useParams()
+
+    const data = [
+        {
+          "name": "Page A",
+          "uv": 4000,
+          "pv": 2400,
+          "amt": 0
+        },
+        {
+          "name": "Page B",
+          "uv": 3000,
+          "pv": 1398,
+          "amt": 0
+        },
+        {
+          "name": "Page C",
+          "uv": 2000,
+          "pv": 9800,
+          "amt": 0
+        },
+        {
+          "name": "Page D",
+          "uv": 2780,
+          "pv": 3908,
+          "amt": 0
+        },
+        {
+          "name": "Page E",
+          "uv": 1890,
+          "pv": 4800,
+          "amt": 0
+        },
+        {
+          "name": "Page F",
+          "uv": 2390,
+          "pv": 3800,
+          "amt": 0
+        },
+        {
+          "name": "Page G",
+          "uv": 3490,
+          "pv": 4300,
+          "amt": 0
+        }
+    ]
+      
 
     return (
         <div id="Country">
             {(!codes.includes(params.code)) && (
                 <Navigate to="/"/>
             )}
+            <h1>{symbols[params.code]}</h1>
             <img src={"flags/" + params.code + ".png"}/>
+
+            <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" dot={false} />
+            </LineChart>
         </div>
     )
 }
--- lambda/data/index.js
+++ lambda/data/index.js
@@ -1,7 +1,6 @@
-import http from 'http'
-import querystring from 'querystring'
+import axios from 'axios'
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm"
-import { DynamoDBClient, PutItemCommand, GetItemCommand } from '@aws-sdk/client-dynamodb'
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb'
 
 const ssm = new SSMClient()
 const dynamodb = new DynamoDBClient()
@@ -11,58 +10,31 @@ const ssm_command = new GetParameterCommand({Name: "currency_exchange_api_access
 const params = {
     'access_key': (await ssm.send(ssm_command)).Parameter.Value
 }
-const params_string = querystring.stringify(params)
-
-const options = {
-  hostname: 'api.exchangeratesapi.io',
-  port: '80',
-  path: '/v1/latest?' + params_string,
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-}
 
 const upload_data = async (json) => {
-    const dynamodb_get_command = new GetItemCommand({
-        'TableName': 'Exchange-Rate-Symbols',
-        'Key': {
-            'key': { S: 'symbols' }
-        }
-    })
-    const symbols = (await dynamodb.send(dynamodb_get_command)).Item
-
-    const items = {
+    const item = {
         timestamp: {S: new Date(json.timestamp * 1000).toISOString().slice(0, 16)},
-        base: {S: json.base},
+        base: {S: json.base}
     }
-    for (let symbol in symbols) {
-        if (symbol !== 'key') {
-            items[symbol] = {N: json.rates[symbol].toString()}
-        }
+
+    for (let symbol in json.rates) {
+        item[symbol] = {N: json.rates[symbol].toString()}
     }
     const dynamodb_put_command = new PutItemCommand({
         'TableName': 'Exchange-Rates',
-        'Item': items
+        'Item': item
     })
    dynamodb.send(dynamodb_put_command)
 }
 
 export const handler = (e) => {
-    let data_json
-    const request = http.request(options, (resp) => {
-        let data = []
-
-        resp.setEncoding('utf8');
-
-        resp.on('data', (chunk) => {
-            data.push(chunk)
-        })
-
-        resp.on('end', () => {
-            data_json = JSON.parse(data)
-            upload_data(data_json)
-        })
+    axios({
+        baseURL: 'http://api.exchangeratesapi.io',
+        url: '/v1/latest',
+        method: 'get',
+        params: params
     })
-    request.end()
+    .then((resp) => {
+        upload_data(resp.data)
+    })
 }
